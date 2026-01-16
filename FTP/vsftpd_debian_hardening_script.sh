d3b7323b467d7fd7d372a70e2a06843992378956
cyberspider217 <47828975+cyberspider217@users.noreply.github.com>
2026-01-15 19:33:41 -0800

    Add files via upload

--- FTP/vsftpd_debian_hardening_script.sh
+++ FTP/vsftpd_debian_hardening_script.sh
@@ -21,6 +21,31 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+# --- DISABLE ANONYMOUS USER IN CASE IT IS ENABLED ---
+echo -e "\n[+] Disabling Anonymous login. The other anonymous options"
+sudo sed -i "s|.*anonymous_enable.*|anonymous_enable=NO|g" /etc/vsftpd.conf
+echo -e "COMPLETE\n"
+
+# --- DISABLE OTHER ANONYMOUS OPTIONS ---
+echo -e "\n[+] Disabling other options relating to anonymous login"
+sudo sed -i "s|.*anon_upload_enable.*|anon_upload_enable=NO|g" /etc/vsftpd.conf
+echo -e "COMPLETE\n"
+
+# --- ALLOW CHROOT (change local root) ---
+echo -e "\n[+] Enabling chroot"
+sudo sed -i "s|.*chroot_local_user.*|chroot_local_user=YES|g" /etc/vsftpd.conf
+echo -e "COMPLETE\n"
+
+# --- ALLOW WRITEABLE CHROOT ---
+echo -e "\n[+] Enabling writeable chroot"
+sudo echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf
+echo -e "COMPLETE\n"
+
+# --- ALLOW WRITES TO FTP ROOT ---
+echo -e "\n[+] Allowing write to FTP root"
+sudo sed -i "s|.*write_enable.*|write_enable=YES|g" /etc/vsftpd.conf
+echo -e "COMPLETE\n"
+
 # --- CREATE USERLIST FILE ---
 echo -e "\n[+] Creating userlist file: /etc/vsftpd.userlist"
 sudo touch /etc/vsftpd.userlist
@@ -28,18 +53,28 @@ sudo chmod 600 /etc/vsftpd.userlist
 echo "[+] Permissions set to root read/write only."
 echo -e "COMPLETE\n"
 
+# --- CREATE FTP ROOT IF IT ISN'T ALREADY MADE ---
+echo -e "\n[+] Creating FTP root in /mnt/files if it doesn't exist already"
+sudo mkdir -p /mnt/Files
+echo -e "COMPLETE\n"
+
 # --- CHANGE FTP ROOT TO /mnt/Files ---
 echo -e "\n[+] Changing FTP root to: /mnt/files"
 # assuming this directory already exists (the previous PDFs on FTP for cybergames implies that the directory, users and files already exist)
-sudo echo "local_root=/mnt/files" | sudo tee -a /etc/vsftpd.conf > /dev/null
+sudo echo "local_root=/mnt/Files" | sudo tee -a /etc/vsftpd.conf > /dev/null
 echo -e "COMPLETE\n"
 
 # --- CHANGE FTP ROOT PERMISSIONS ---
+echo -e "\n[+] Changing FTP root directory permissions"
 sudo chown -R nobody:ftp_users /mnt/Files # should change the permissions of /mnt/Files to nobody:ftp_users AND all of the contents within it 
 	# to be owned by nobody and group as ftp_users 
 sudo chmod 2070 /mnt/Files
+echo -e "COMPLETE\n"
 
-# TODO: make arbitrary check that changes all of the owners and groups for the existing files within FTP root. Include hidden files too
+# --- MAKE SURE FTP_USERS GROUP CAN READ FILES CREATED BY SCORING USERS ---
+echo -e "\n[+] Making sure ftp_users group can read files created by other scoring users"
+sudo echo "local_umask=007" >> /etc/vsftpd.conf
+echo -e "COMPLETE\n"
 
 # --- READ AND APPEND USERLIST ENTRIES ---
 filename="$1"
